8cea1645f6ce479e3fc4a5512173eb34e233b6af
Vaibhav Kumar <vaibhavkumarsvs@gmail.com>
2021-06-10 08:01:10 +0530

    Update mongo.py

--- movie/mongo.py
+++ movie/mongo.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from pprint import pprint
 
-client = MongoClient("mongodb+srv://movie:movie@vaibhav123-sx5zi.mongodb.net/test?retryWrites=true")
+client = MongoClient("")
 db = client.recommender
 
 movies = db.movies.find({}).sort([("movieId", 1)])
